2d09ff05f125c75a95ff53074c29bc86023e4767
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-12-08 15:00:49 -0800

    Updated with HowlerVue and automatically starting music

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+// window.onload = function () {
+//     var pDBtn = document.getElementById("ParadiseLightsbtn");
+//     pDBtn.onclick = paraD();
+// }
+
+
+    var sound = new Howl({
+        src: ['Sounds/ParadiseLights.mp3']
+        });
+
+    sound.play();
+
+
+
+
+
 // import {Howl, Howler} from 'howler';
 // const {Howl, Howler} = require('howler');
 
 
 
-// var sound = new Howl({
-//     src: ['Sounds/grossDub/mp3']
-// });
-
-// sound.play();
