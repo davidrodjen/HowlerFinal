11e9485992e151306806938165f3bc876e4fbbec
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-12-08 15:35:34 -0800

    Setup Two Buttons to play two different song bites

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
-// window.onload = function () {
-//     var pDBtn = document.getElementById("ParadiseLightsbtn");
-//     pDBtn.onclick = paraD();
-// }
+window.onload = function () {
+    var pDBtn = document.getElementById("ParadiseLightsbtn");
+    pDBtn.onclick = ParadisePlay;
+
+    var dubBtn = document.getElementById("BadDub");
+    dubBtn.onclick = dubPlay;
+}
 
 
-    var sound = new Howl({
-        src: ['Sounds/ParadiseLights.mp3']
-        });
+var Paradisesound = new Howl({
+    src: ['Sounds/ParadiseLights.mp3'],
+    autoplay: false,
+    loop: false,
+    volume: 0.3,
+    onend: function (){
+        console.log('Finished');
+    }
+    });
 
-    sound.play();
+var Dubsound = new Howl({
+    src: ['Sounds/grossDub.mp3'],
+    autoplay: false,
+    loop: false,
+    volume: 0.3,
+    oneend: function() {
+        console.log('Finished');
+    }
+});
+    
+function ParadisePlay(){
+    Paradisesound.play();
+}
+
+function dubPlay(){
+    Dubsound.play();
+}
+// sound.play();
+
 
 
 
